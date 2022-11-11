fe293f7be7cd6726d88d205b7ee6a842ee39ee97
Dmitri Popov <dmpop@linux.com>
2022-11-11 13:52:17 +0100

    geocorrelate

---
+++ geocorrelate.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+if [ ! -x "$(command -v exiftool)" ]; then
+    echo "Install ExifTool."
+    exit 1
+fi
+
+if [ -f "$2" ]; then
+    exiftool -overwrite_original -geotag "$2" -geosync=180 "$1" >>"$PREFIX/tmp/geocorrelate.log" 2>&1
+fi
