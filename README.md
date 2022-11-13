8cc63597c092914790ab7d8fec80712493854df0
Dmitri Popov <dmpop@linux.com>
2022-11-13 12:42:15 +0100

    Fix img-up, README, LICENSE

---
+++ README.md
@@ -0,0 +1,18 @@
+# Termux shell scripts
+
+Simple shell scripts examples used in the [Linux Photography](https://gumroad.com/l/linux-photography) book.
+
+<img src="https://tokyoma.de/bookcovers/linux-photography.jpg" title="Linux Photography book" width="200"/>
+
+## Problems?
+
+Please report bugs and issues in the [Issues](https://github.com/dmpop/termux-scripts/issues) section.
+
+## Author
+
+Dmitri Popov [dmpop@linux.com](mailto:dmpop@tokyoma.de)
+
+## License
+
+The [GNU General Public License version 3](http://www.gnu.org/licenses/gpl-3.0.en.html)
+
--- img-up.sh
+++ img-up.sh
@@ -3,15 +3,16 @@
 size="1024"
 quality="99"
 remote_user="foo"
+remote_pass="secret"
 remote_host="hello.xyz"
 remote_path="/var/www/html"
 
-if [ ! -x "$(command -v mogrify)" ] || [ ! -x "$(command -v jpegoptim)" ]; then
-    echo "Install jhead and jpegoptim"
+if [ ! -x "$(command -v mogrify)" ] || [ ! -x "$(command -v jpegoptim)" ] || [ ! -x "$(command -v sshpass)" ]; then
+    echo "Install jhead, jpegoptim, sshpass"
     exit 1
 fi
 
 jhead -n%Y%m%d-%H%M%S *.JPG
 for file in *.JPG; do jpegoptim $file "`basename $file .jpg`.jpeg"; done
 for file in *.jpeg; do mogrify -resize "$size>" -quality "$quality%" $file; done
-for file in *.jpeg; do scp $file "$remote_user@$remote_server:$remove_path"; done
+for file in *.jpeg; do sshpass -p "$remote_pass" scp $file "$remote_user@$remote_server:$remove_path"; done
