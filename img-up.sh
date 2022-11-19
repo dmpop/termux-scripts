48be2bcd81ecd1c602f17bd894212b86418cb52c
Dmitri Popov <dmpop@linux.com>
2022-11-19 18:11:33 +0100

    Update img-up.sh

--- img-up.sh
+++ img-up.sh
@@ -1,18 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-size="1024"
-quality="99"
+wrk_dir="storage/dcim/Camera"
 remote_user="foo"
 remote_pass="secret"
 remote_host="hello.xyz"
 remote_path="/var/www/html"
 
 if [ ! -x "$(command -v mogrify)" ] || [ ! -x "$(command -v jpegoptim)" ] || [ ! -x "$(command -v sshpass)" ]; then
-    echo "Install jhead, jpegoptim, sshpass"
+    echo "Install jhead, imagemagick, sshpass"
     exit 1
 fi
 
-jhead -n%Y%m%d-%H%M%S *.JPG
-for file in *.JPG; do jpegoptim $file "`basename $file .jpg`.jpeg"; done
-for file in *.jpeg; do mogrify -resize "$size>" -quality "$quality%" $file; done
-for file in *.jpeg; do sshpass -p "$remote_pass" scp $file "$remote_user@$remote_server:$remove_path"; done
+cd $wrk_dir
+mkdir -p results
+for file in *; do convert $file -sampling-factor 4:2:0 -strip -quality 85 -interlace JPEG -colorspace RGB results/$file; done
+cd results
+jhead -n%Y%m%d-%H%M%S *
+for file in *; do sshpass -p "$remote_pass" scp $file "$remote_user@$remote_server:$remove_path"; done
