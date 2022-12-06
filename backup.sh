0a01096c3ff2494d4a4460b7ad376d0f9b94b650
Dmitri Popov <dmpop@linux.com>
2022-12-06 12:04:53 +0100

    Backup script fixes

--- backup.sh
+++ backup.sh
@@ -1,14 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+local_dr="/path/to/source/dir"
 remote_user="foo"
-remote_host="hello.xyz"
-remote_pass="secret"
-local_path="/path/to/source/dir"
-remote_path="/path/to/remove/dir"
+remote_server="hello.xyz"
+remote_passwd="secret"
+remote_dir="/path/to/remote/dir"
 
 if [ ! -x "$(command -v rsync)" ] || [ ! -x "$(command -v sshpass)" ]; then
     echo "Install rsync and sshpass"
     exit 1
 fi
 
-sshpass -p "$remote_pass" rsync -avhz --delete -P -e "ssh -p 22" "$user@$remote_server:$remote_path/ $local_path"
+sshpass -p "$remote_passwd" rsync -avhz --exclude=".*" --delete -P -e "ssh -p 22" "$local_dir/" "$user"@"$remote_server":"$remote_dir"
